aef0783259a0df5a25ce01774bf95c4f066db050
Răzvan Panda <razvan.panda@gmail.com>
2014-01-02 02:48:07 +0200

    TrayIcon image and right click menu text

--- src/main/scala/ApplicationGui.scala
+++ src/main/scala/ApplicationGui.scala
@@ -9,14 +9,24 @@ import scalafx.scene
 import scene.effect._
 import scene.paint.{Stops, LinearGradient}
 import scene.text.Text
+import javafx.application.Platform
+import java.awt._
+import javafx.stage.{WindowEvent, Stage}
+import javax.imageio.ImageIO
+import java.io.File
+import javafx.event.EventHandler
+import java.awt.event.{ActionEvent, ActionListener}
 
 object ApplicationGui extends JFXApp
 {
+    private var firstTime = false
+    private var trayIcon: TrayIcon = null
+
     stage = new PrimaryStage
     {
         title = "RealityQuest"
         width = 600
-        height = 450
+        height = 170
         resizable = false
         scene = new Scene
         {
@@ -48,5 +58,94 @@ object ApplicationGui extends JFXApp
         }
     }
 
+    def createTrayIcon(stage: Stage)
+    {
+        if (SystemTray.isSupported)
+        {
+            val tray: SystemTray = SystemTray.getSystemTray
+            var image: Image = null
+            image = ImageIO.read(new File("src/main/resources/TrayIcon.png"))
+
+            stage.setOnCloseRequest(new EventHandler[WindowEvent]
+            {
+                def handle(t: WindowEvent)
+                {
+                    hide(stage)
+                }
+            })
+
+            val closeListener: ActionListener = new ActionListener
+            {
+                def actionPerformed(e: ActionEvent)
+                {
+                    System.exit(0)
+                }
+            }
+
+            val showListener: ActionListener = new ActionListener
+            {
+                def actionPerformed(e: ActionEvent)
+                {
+                    Platform.runLater(new Runnable
+                    {
+                        def run()
+                        {
+                            stage.show()
+                        }
+                    })
+                }
+            }
+
+            val popup: PopupMenu = new PopupMenu
+            val showItem: MenuItem = new MenuItem("Show")
+            showItem.addActionListener(showListener)
+            popup.add(showItem)
+            val closeItem: MenuItem = new MenuItem("Close")
+            closeItem.addActionListener(closeListener)
+            popup.add(closeItem)
+            trayIcon = new TrayIcon(image, "Title", popup)
+            trayIcon.setImageAutoSize(true)
+            trayIcon.addActionListener(showListener)
+            try
+            {
+                tray.add(trayIcon)
+            }
+            catch
+            {
+                case e: AWTException => System.err.println(e)
+            }
+        }
+    }
+
+    private def hide(stage: Stage)
+    {
+        Platform.runLater(new Runnable
+        {
+            def run()
+            {
+                if (SystemTray.isSupported)
+                {
+                    stage.hide()
+                    if (firstTime)
+                    {
+                        trayIcon.displayMessage(
+                            "Minimized to tray.",
+                            """Double click to open.
+                              |Right click for context menu.""".stripMargin,
+                            TrayIcon.MessageType.INFO)
+                        firstTime = false
+                    }
+                }
+                else
+                {
+                    System.exit(0)
+                }
+            }
+        })
+    }
+
+    createTrayIcon(stage)
+    firstTime = true
+    Platform.setImplicitExit(false)
     new Main
 }
